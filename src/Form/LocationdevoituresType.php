7399cd0fb4fb876f5f213a3ebc33ecbb0e7574f6
Bilal-Choukri <Bilal.choukri77@gmail.com>
2021-08-19 10:20:19 +0200

    7eme commit

--- src/Form/LocationdevoituresType.php
+++ src/Form/LocationdevoituresType.php
@@ -2,16 +2,19 @@
 
 namespace App\Form;
 
+use DateTime;
+
 use App\Entity\LocationDeVoitures;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class LocationDeVoituresType extends AbstractType
 {
